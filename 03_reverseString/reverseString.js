174676de5bf9f6e85fd972dafc4d39b95500ba28
gbalcha <29507077+gutemberg33@users.noreply.github.com>
2024-12-04 23:49:01 +0100

    Refactor function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
+let stringToReturn = '';
 
+const reverseString = function(stringToReverse) {
+    const stringArray = stringToReverse.split("").reverse();
+    stringToReturn = stringArray.join("");
+    return stringToReturn;
 };
 
 // Do not edit below this line
