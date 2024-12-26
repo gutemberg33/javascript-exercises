aecf37ebf7bc16a1e055bb79da86a800f062c177
gbalcha <29507077+gutemberg33@users.noreply.github.com>
2024-12-26 18:50:24 +0100

    Add logic to functions and test it in Jest.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (numArrayOne) {
+  if (numArrayOne.length === 0) {
+    return 0;
+  } else if (numArrayOne.length === 1) {
+    return numArrayOne[0];
+  } else if (numArrayOne.length === 2) {
+    return numArrayOne[0] + numArrayOne[1];
+  } else {
+    return numArrayOne.reduce((total, currentItem) => {
+      return total + currentItem;
+    }, 0);
+  }
 };
 
-const multiply = function() {
-
+const multiply = function (numberArray) {
+  if (numberArray.length >= 2) {
+    return numberArray.reduce((total, currentItem) => {
+      return total * currentItem;
+    }, 1);
+  }
 };
 
-const power = function() {
-	
+const power = function (numOne, numTwo) {
+  return Math.pow(numOne, numTwo);
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+  let res = 1;
+
+  if (n === 0)
+    return 1;
+  for (let i = 2; i <= n; i++)
+    res = res * i;
+  return res;
 };
 
 // Do not edit below this line
