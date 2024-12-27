57e139565d9a7034ddde0a5c84c992b35831064f
gbalcha <29507077+gutemberg33@users.noreply.github.com>
2024-12-27 02:05:06 +0100

    Add logic to function and test it in Jest.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,12 @@
-const palindromes = function () {
-
-};
-
-// Do not edit below this line
-module.exports = palindromes;
+const palindromes = function (stringWord) {
+    // Remove all non-alphanumeric characters and spaces
+    const cleanedString = stringWord.toLowerCase().replace(/[^a-z0-9]/g, '');
+    
+    // Reverse the cleaned string and compare with the original cleaned string
+    const reversed = cleanedString.split('').reverse().join('');
+    
+    return cleanedString === reversed;
+  };
+  
+  // Do not edit below this line
+  module.exports = palindromes;  
